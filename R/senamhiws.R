11434643485696da511e0a359c8262c71e976c9e
GeomarPerales <perales.geomar@gmail.com>
2023-07-30 07:47:22 -0500

    1

---
+++ R/senamhiws.R
@@ -0,0 +1,18 @@
+#' function for processing of pisco daily values to a monthly average.
+#'
+#' function for processing of pisco daily values to a monthly average, apply to precipitation,
+#' temperature and evapotranspiration.
+#' @param x a dataframe with PISCO daily values.
+#' @param param OPTIONAL, default parameter is sum for precipitation and evapotranspiration,
+#' use mean for temperature.
+#'
+#'@export
+#'
+#' @author Geomar Perales Apaico
+#'
+#' @name monthlyavg
+
+
+senamhiws <- function() {
+  print("Hello, world!")
+}
