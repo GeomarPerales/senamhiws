f4729eb17d9ce2763a7bd8572dd8f6ab4e7b4e6c
GeomarPerales <perales.geomar@gmail.com>
2023-07-30 08:57:38 -0500

    1

--- R/senamhinfo.R
+++ R/senamhinfo.R
@@ -1,18 +1,56 @@
-#' function for processing of pisco daily values to a monthly average.
+#' function for get stations information of SENAMHI.
 #'
-#' function for processing of pisco daily values to a monthly average, apply to precipitation,
-#' temperature and evapotranspiration.
-#' @param x a dataframe with PISCO daily values.
-#' @param param OPTIONAL, default parameter is sum for precipitation and evapotranspiration,
-#' use mean for temperature.
+#' function for get information (coordinates, station code and altitude) about stations.
+#' @param x a vector from stations code
+#' @param stations data of SENAMHI web scraping
 #'
-#'@export
+#' @import rvest
+#'
+#' @export
 #'
 #' @author Geomar Perales Apaico
 #'
 #' @name senamhinfo
 
+senamhinfo <- function(x, stations) {
+  cod_stn <- list()
+  read_snm <- list()
+  df_idx_stn <- list()
+  scraping_web_senamhi <- list()
+  data_stn_senamhi <- list()
 
-senamhinfo <- function() {
-  print("Hello, world!")
+  for (i in 1:length(x)) {
+    cod_stn[[i]] <- x[i]
+    idx.cod <- grep(as.character(cod_stn[[i]]), stations$cod)
+    df_idx_stn[[i]] <- data.frame(stations[idx.cod,], stringsAsFactors = FALSE)
+
+    if(is.na(df_idx_stn[[i]]$cod)){
+      link <- paste0("https://www.senamhi.gob.pe//mapas/mapa-estaciones-2/_dato_esta_tipo02.php?estaciones=",
+                     as.character(df_idx_stn[[i]]$cod), #cod estacion
+                     "&CBOFiltro=",
+                     "202401", #fecha año y mes
+                     "&t_e=",
+                     as.character(df_idx_stn[[i]]$ico), #ico
+                     "&estado=",
+                     as.character(df_idx_stn[[i]]$estado),
+                     "&cod_old=", as.character(df_idx_stn[[i]]$cod_old)) #estado
+
+    } else {
+      link <- paste0("https://www.senamhi.gob.pe//mapas/mapa-estaciones-2/_dato_esta_tipo02.php?estaciones=",
+                     as.character(df_idx_stn[[i]]$cod), #cod estacion
+                     "&CBOFiltro=",
+                     "202401", #fecha año y mes
+                     "&t_e=",
+                     as.character(df_idx_stn[[i]]$ico), #ico
+                     "&estado=",
+                     as.character(df_idx_stn[[i]]$estado)) #estado
+    }
+    data_stn_senamhi <- read_html(link)
+    scraping_web_senamhi[[i]] <- html_table(data_stn_senamhi)
+    data_stn_senamhi[[i]] <- scraping_web_senamhi[[i]][[1]]
+    df_dss <- data.frame(data_stn_senamhi[[i]])
+    df_idx_stn[[i]]$alt <- strsplit(df_dss$X6[3], " ")[[1]][1]
+  }
+  data_stn_senamhi <- do.call("rbind", df_idx_stn)
+  return(data_stn_senamhi)
 }
--- R/stations.R
+++ R/stations.R
@@ -1,10 +1,6 @@
-#' function for processing of pisco daily values to a monthly average.
+#' function for get all stations information of SENAMHI web scraping
 #'
-#' function for processing of pisco daily values to a monthly average, apply to precipitation,
-#' temperature and evapotranspiration.
-#' @param x a dataframe with PISCO daily values.
-#' @param param OPTIONAL, default parameter is sum for precipitation and evapotranspiration,
-#' use mean for temperature.
+#' function for get all stations information (coordinates and altitude) of SENAMHI web scraping.
 #'
 #'@export
 #'
@@ -14,5 +10,49 @@
 
 
 stations <- function() {
-  print("Hello, world!")
+  link <- "https://www.senamhi.gob.pe/mapas/mapa-estaciones-2/"
+  stn_senamhi <- read_html(link)
+
+  stn_senamhi2 <- strsplit(html_text(stn_senamhi), "nom")
+  stn_senamhi2[[1]] <- stn_senamhi2[[1]][-c(1)]
+
+  stn <- list()
+  cat <- list()
+  lat <- list()
+  lon <- list()
+  ico <- list()
+  cod <- list()
+  cod_old <- list()
+  estado <- list()
+  data_stn <- list()
+
+  for (i in 1:length(stn_senamhi2[[1]])) {
+    x <- gsub('\"',"", stn_senamhi2[[1]][i])
+    x <- gsub(': ', ":", x)
+    x <- gsub(',\n', "", x)
+    x <- gsub('\\}\\{', "", x)
+    data_estaciones <- strsplit(x, ",")
+    stn[[i]] <- gsub(":", "", data_estaciones[[1]][1])
+    cat[[i]] <- gsub("cate:", "", data_estaciones[[1]][2])
+    lat[[i]] <- gsub("lat:", "", data_estaciones[[1]][3])
+    lon[[i]] <- gsub("lon:", "", data_estaciones[[1]][4])
+    ico[[i]] <- gsub(" ico:", "", data_estaciones[[1]][5])
+
+    cod[[i]] <- ifelse(substr(data_estaciones[[1]][6], 1, 5) == " cod:",
+                       gsub(" cod:", "",data_estaciones[[1]][6]), NA)
+
+    cod_old[[i]] <- ifelse(substr(data_estaciones[[1]][7], 1, 8) == "cod_old:",
+                           gsub("cod_old:", "",data_estaciones[[1]][7]), NA)
+
+    estado[[i]] <- ifelse(is.na(data_estaciones[[1]][8]),
+                          ifelse(substr(data_estaciones[[1]][7], 1, 8) == " estado:", gsub(" estado:", "",data_estaciones[[1]][7]), NA),
+                          ifelse(substr(data_estaciones[[1]][8], 1, 8) == " estado:", gsub(" estado:", "",data_estaciones[[1]][8]), NA))
+
+    data_stn[[i]] <- data.frame(estacion = stn[[i]], categoria = cat[[i]],
+                                lat = lat[[i]], lon = lon[[i]], ico = ico[[i]],
+                                cod = cod[[i]], cod_old = cod_old[[i]], estado = estado[[i]])
+  }
+
+  df_stns <- do.call("rbind", data_stn)
+  return(df_stns)
 }
